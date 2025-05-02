d5faab4eb063f941b6c8e29250d1637171dd806e
germannaa <reboucasgermanna@gmail.com>
2025-05-02 16:18:14 -0300

    Primeiro commit

---
+++ MB_RBS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MB_RBS
+//
+//  Created by Rennan Rebouças  on 02/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
